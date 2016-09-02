66d193da2c1d17777f0b85b7f8f39c68b0931d5a
HelloJundy <hellojundy@163.com>
2016-09-02 22:26:12 +0800

    add advanced setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
+# coding: utf-8
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+
+# 引入minitest 为了让Rails应用的测试适时变红变绿
+require 'minitest/reporters'
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
